ffa1b62c73ce249ff373e946f27a5027c2d7de19
Gürol <gurolcakir1@gmail.com>
2025-02-01 02:34:04 +0300

    fast api ilk uygulama

---
+++ route-example1/main.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+
+async def root():
+    return {"message": "hello world"}
+
+@app.post("/")
+async def post():
+    return {"message": "hello from the post route"}
+
+
+
+@app.put("/")
+async def put():
+
+        return {"messsage": " Put , rotası"}
+
+
+
