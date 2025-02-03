a8d0aabc8065a22a44d8f42729456d8c417a50ac
Gürol <gurolcakir1@gmail.com>
2025-02-04 01:08:33 +0300

    sorgu parametreleri ile işlemler

--- route-example1/main.py
+++ route-example1/main.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Optional
 
 from fastapi import FastAPI
 
@@ -48,3 +49,29 @@ async def get_food(food_name: FoodEnum):
         return {"foodname": food_name , "Messsage ": "you are still healthy, but like sweet things"}
 
     return {"foodname": food_name , "Messsage ": "çikolatalı süt içerim"}
+
+
+fake_items_db = [{"items_name": "Foo"},{"items_name": "Bass"},{"items_name": "Bazz"}]
+
+@app.get("/items")
+async def list_items(skip: int = 0 , limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+@app.get("/items/{item_id}")
+async def get_item(item_id: str, sample_query_param: str, q: Optional[str] = None, short: bool = False):
+    item = {"item_id": item_id , "sample_query_param": sample_query_param} 
+    if q:
+        item.update({"q":q})
+    if not short:
+        item.update({"description": "Deneme açıklama lorem ipsum"})
+    return item
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def get_user_item(user_id: int, item_id: str, q: str | None = None, short: bool= False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q":q})
+    if not short:
+        item.update({"description": "Deneme açıklama lorem ipsum"})
+    return item
